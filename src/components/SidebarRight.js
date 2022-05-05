ae6992a1be9ef306d8c38e4b1db4d8ece8e1c363
Craig Leslie <cleslie25@gmail.com>
2022-05-05 16:11:06 -0500

    added back the icons in the public folder

--- src/components/SidebarRight.js
+++ src/components/SidebarRight.js
@@ -6,7 +6,7 @@ export const SidebarRight = () => {
     <aside className='sidenav-rt'>
         <Link className='icon-rt' to="/projects">Projects</Link>
         <Link className='icon-rt' to="/about">Bio</Link>
-        <a className='icon-rt' href='https://docs.google.com/document/d/1OwXVNU7gS-IJWlV06PLVgitHxMju1cnCMszprEaawv0/edit?usp=sharing'>Resume</a>
+        <a className='icon-rt' href='https://docs.google.com/document/d/1OwXVNU7gS-IJWlV06PLVgitHxMju1cnCMszprEaawv0/export?format=pdf'>Resume</a>
     </aside>
   )
 };
