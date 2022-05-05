2f1e6e8b10ff4b327bb7ae5d91d2b4b12e1c9c0b
Craig Leslie <cleslie25@gmail.com>
2022-05-05 13:38:56 -0500

    still trying to deploy properly

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,12 @@
-import './assets/home.css'
+import './assets/home.css';
 import Header from './Header';
 import { SidebarLeft } from './SidebarLeft';
 import { SidebarRight } from './SidebarRight';
 
-// const picture = require('./assets/meandheights.jpeg')
 
 const Home = () => {
   return (
     <div className='homepage'>
-      {/* <div className='name'>
-        <h1><span className='spaceName'>C r a i g</span> <span>L e s l i e</span></h1>
-        <h3>Software Engineer</h3>
-      </div> */}
       <Header title={'name'}/>
       <SidebarLeft />
       <SidebarRight />
